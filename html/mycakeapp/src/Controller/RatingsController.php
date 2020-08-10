701036b11560a2c1de30a6d09dd347ab494608e8
39nina <392mknn@gmail.com>
2020-08-10 20:37:41 +0900

    Fix：取引ページの評価機能をRatingsコントローラーに移動

--- html/mycakeapp/src/Controller/RatingsController.php
+++ html/mycakeapp/src/Controller/RatingsController.php
@@ -46,4 +46,95 @@ class RatingsController extends AuctionBaseController
         $this->set(compact('avg'));
     }
 
+	public function rating($bidinfo_id = null)
+	{
+		// $bidinfo_idからBidinfoを取得する
+		try {
+			$bidinfo = $this->Bidinfo->get($bidinfo_id, ['contain'=>['Biditems']]);
+		} catch(Exception $e){
+			$bidinfo = null;
+		}
+		$bidinfo_id = $bidinfo->id;
+
+		// 落札者idと出品者idを用意
+		$bidder_id = $bidinfo->user_id;
+		$exhibitor_id = $bidinfo->biditem->user_id;
+		$this->set(compact('bidder_id', 'exhibitor_id'));
+
+		// 取引連絡開始用に落札者の発送先情報連絡フォームを用意
+		// このオークションの取引連絡レコードが作られている場合、$contactEntityを設定
+		$biditem_id = $bidinfo->biditem_id;
+		try {
+			$contactEntity = $this->Contacts->find('all',
+				 ['conditions'=>['biditem_id'=>$biditem_id]])->first();
+		} catch(Exception $e) {
+			$contactEntity = null;
+		}
+		$this->set(compact('contactEntity'));
+
+		// 落札者から受け取り評価連絡がPOSTされた場合
+		if (!empty($this->request->getData('rate'))) {
+
+			// ratingsテーブルに新レコードを作成する
+			$rating = $this->Ratings->newEntity();
+			// 送信された内容でエンティティを更新、追加情報を$ratingにセット
+			$rating = $this->Ratings->patchEntity($rating, $this->request->getData());
+			$rating->biditem_id = $bidinfo['biditem_id'];
+			$rating->rater_id = $bidder_id;
+			$rating->rate_target_id = $exhibitor_id;
+			$rating->rate = $this->request->getData('rate');
+			$rating->comment = $this->request->getData('comment');
+			// 落札者からの評価フラグに1をセット
+			$contactEntity->is_rated_by_bidder = 1;
+			// $Ratingを保存
+			if ($this->Ratings->save($rating) && $this->Contacts->save($contactEntity)) {
+				//重複送信防止
+				header('Location: ./' . $bidinfo_id);
+			} else {
+				$this->Flash->error(__('送信に失敗しました。'));
+			}
+		}
+
+		// 出品者から受け取り評価連絡がPOSTされた場合
+		if (!empty($this->request->getData('rate2'))) {
+			// ratingsテーブルに新レコードを作成する
+			$rating2 = $this->Ratings->newEntity();
+			// 送信された内容でエンティティを更新、追加情報を$ratingにセット
+			$rating2 = $this->Ratings->patchEntity($rating2, $this->request->getData());
+			$rating2->biditem_id = $bidinfo['biditem_id'];
+			$rating2->rater_id = $exhibitor_id;
+			$rating2->rate_target_id = $bidder_id;
+			$rating2->rate = $this->request->getData('rate2');
+			$rating2->comment = $this->request->getData('comment');
+			// 落札者からの評価フラグに1をセット
+			$contactEntity->is_rated_by_exhibitor = 1;
+			// $Ratingを保存
+			if ($this->Ratings->save($rating2) && $this->Contacts->save($contactEntity)) {
+				//重複送信防止
+				header('Location: ./' . $bidinfo_id);
+			} else {
+				$this->Flash->error(__('送信に失敗しました。'));
+			}
+		}
+
+		// Bidmessageを新たに用意
+		$bidmsg = $this->Bidmessages->newEntity();
+		// $bidmsg（メッセージ）がPOSTされた時の処理
+		if ($this->request->is('post') && !empty($this->request->getData('message'))) {
+			// 送信されたフォームで$bidmsgを更新
+			$bidmsg = $this->Bidmessages->patchEntity($bidmsg, $this->request->getData());
+			// Bidmessageを保存
+			if ($this->Bidmessages->save($bidmsg)) {
+				$this->Flash->success(__('保存しました。'));
+			} else {
+				$this->Flash->error(__('保存に失敗しました。もう一度入力下さい。'));
+			}
+		}
+		// Bidmessageをbidinfo_idとuser_idで検索
+		$bidmsgs = $this->Bidmessages->find('all',[
+			'conditions'=>['bidinfo_id'=>$bidinfo_id],
+			'contain' => ['Users'],
+			'order'=>['created'=>'desc']]);
+		$this->set(compact('bidmsgs', 'bidinfo', 'bidmsg'));
+	}
 }
--- html/mycakeapp/src/Template/Auction/contact.ctp
+++ html/mycakeapp/src/Template/Auction/contact.ctp
@@ -77,41 +77,6 @@
 		<?php endif; ?>
 	<?php endif; ?>
 
-	<!-- 配送状況 -->
-	<?php if ($contactEntity['is_rated_by_bidder'] ===  true ): ?>
-		<h3>【取引状況】</h3>
-		<!-- ログイン者が落札者で、落札者のみ評価終了している場合のみ表示 -->
-		<?php if ($contactEntity['is_rated_by_exhibitor'] ===  false && $bidder_id === $authuser['id']): ?>
-			<p>※ 受取評価が完了しました。出品者からの評価をお待ちください。</p>
-		<?php endif; ?>
-		<!-- ログイン者が出品者で、落札者のみ評価終了している場合のみ表示 -->
-		<?php if ($contactEntity['is_rated_by_exhibitor'] ===  false && $exhibitor_id === $authuser['id']): ?>
-			<p>※ 落札者が商品を受け取りました。落札者の評価をしてください。</p>
-			<?= $this->Form->create('',[
-			'enctype' => 'multipart/form-data',
-			'type' => 'post'
-			]) ?>
-			<fieldset>
-				<legend>落札者の取引評価を入力：</legend>
-				<?php
-					echo 'rate';
-					echo $this->Form->select('rate2',[1=>'1（とても悪い）', 2=>'2（悪い）', 3=>'3（普通）', 4=>'4（良い）', 5=>'5（とても良い）'],['default'=>3]);
-					echo $this->Form->control('comment',[
-						'type' => 'textarea',
-						'maxlength' => 200
-					]);
-				?>
-			</fieldset>
-			<?= $this->Form->button(__('評価をする')) ?>
-			<?= $this->Form->end() ?>
-		<?php endif; ?>
-		<!-- ログイン者が落札者か出品者で、両者の評価が完了した場合のみ表示 -->
-		<?php if ($contactEntity['is_rated_by_exhibitor'] ===  true): ?>
-		<p>※ 取引が完了しました。</p>
-		<?php endif; ?>
-		<br>
-	<?php endif; ?>
-
 	<!-- 発送先情報が送信された後の表示 -->
 	<!-- ログイン者が落札者・出品者どちらの場合も表示 -->
 	<?php if (!empty($contactEntity)): ?>
